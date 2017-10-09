fa1d89e8e15f0bc9d12416b20b9840a3c4b15263
jurejoy <jurejoy@foxmail.com>
2017-10-09 18:53:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello container World!");
+    response.end("Hello bloody World!");
 
 });
 
